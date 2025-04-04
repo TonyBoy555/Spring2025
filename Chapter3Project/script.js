6fb5510ce5a1bca7f4c04c14a257821714780944
TonyBoy555 <aperezar@stumail.jccc.edu>
2025-04-04 16:29:11 -0500

    Create script.js

---
+++ Chapter3Project/script.js
@@ -0,0 +1,22 @@
+//Anthony Perez 
+
+var name = "Anthony Perez";
+console.log(name);
+
+var annualSalary = 100000;
+console.log(annualSalary);
+
+var vetStatus = false;
+console.log(vetStatus);
+
+const friends = [ "Andrea", "Logan", "Jacob"];
+console.log(friends);
+
+const salaries = [1000000, 24000, 50000];
+console.log(salaries);
+
+const object = {
+    firstName: "JJ",
+    lastName: "Andrade",
+    salary: 100000
+};
